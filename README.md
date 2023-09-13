342dac93268698ebe830006fd1614adb2b77018c
preciousnjukia <preciousnjukia@gmail.com>
2023-09-13 21:48:48 +0300

    Changes made

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# clinic-app
+# Clinic-app
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,29 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar(){
+    return (
+    <nav className="navbar">
+     <h1>Clinic App</h1>
+      <div className="navbar-logo">
+        <NavLink to="/">Clinic App</NavLink>
+      </div>
+      <ul className="navbar-menu">
+        <li>
+          <NavLink to="/book-appointment">Book Appointment</NavLink>
+        </li>
+        <li>
+          <NavLink to="/about-us">About Us</NavLink>
+        </li>
+        <li>
+          <NavLink to="/login">Login</NavLink>
+        </li>
+        <li>
+          <NavLink to="/sign-up">Sign Up</NavLink>
+        </li>
+      </ul>
+      <button className="consult-button">Consult Online</button>
+    </nav>
+  );
 
 }
-export default NavBar
+export default NavBar;
