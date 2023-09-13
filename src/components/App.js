ae74f562121fb82bafa323e5a7ab5298225bbf5c
preciousnjukia <preciousnjukia@gmail.com>
2023-09-14 01:39:13 +0300

    More changes

--- src/components/App.js
+++ src/components/App.js
@@ -5,20 +5,24 @@ import NavBar from "./NavBar";
 import DoctorsSection from "./DoctorsSection";
 import FeedBack from "./FeedBack";
 import Footer from "./Footer";
+import AboutUs from "./AboutUs";
 
 function App(){
   return (
     <Router>
+      <div>
+       <NavBar />
       <Switch>
         <Route path="/navbar" exact component={NavBar} />
         <Route path="/doctorssection" exact component={DoctorsSection} />
         <Route path="/feedback" exact component={FeedBack} />
         <Route path="/footer" exact component={Footer} />
+        <Route path="/aboutus" exact component={AboutUs} />
       </Switch>
+      </div>
     </Router>
   );
 }
 
 
 export default App;
-
