cb948673e8a5e655d13a5289f101c9a1614c9e0a
preciousnjukia <preciousnjukia@gmail.com>
2023-09-14 03:08:32 +0300

    More changes

---
+++ src/components/BookAppointment.js
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+
+function BookingAppointment() {
+  const [appointmentData, setAppointmentData] = useState({
+    fullName: "",
+    email: "",
+    appointmentDate: "",
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setAppointmentData({
+      ...appointmentData,
+      [name]: value,
+    });
+};
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  const { fullName, email, appointmentDate } = appointmentData;
+
+  const appointment = {
+    fullName,
+    email,
+    appointmentDate,
+  };
+
+  fetch("/src/appointments.json", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(appointment),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+        alert("Appointment booked successfully!");
+    })
+    .catch((error) => {
+      console.error("Error booking appointment:", error);
+      alert("Failed to book the appointment. Please try again later.");
+    });
+};
+
+return (
+  <div>
+    <h2>Book Appointment</h2>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Full Name:</label>
+        <input
+          type="text"
+          name="fullName"
+          value={appointmentData.fullName}
+          onChange={handleInputChange}
+          required
+        />
+      </div>
+      <div>
+        <label>Email:</label>
+        <input
+          type="email"
+          name="email"
+          value={appointmentData.email}
+          onChange={handleInputChange}
+          required
+        />
+      </div>
+      <div>
+        <label>Appointment Date:</label>
+        <input
+          type="date"
+          name="appointmentDate"
+          value={appointmentData.appointmentDate}
+          onChange={handleInputChange}
+          required
+        />
+      </div>
+      <button type="submit">Book Appointment</button>
+    </form>
+  </div>
+);
+}
+
+export default BookingAppointment;
+
+
---
+++ src/components/Login.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+
+function Login() {
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setLoginData({
+      ...loginData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const { email, password } = loginData;
+
+    const userCredentials = {
+        email,
+        password,
+      };
+
+      fetch("/src/user.json", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userCredentials),
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+            alert("Login successful!");
+        })
+        .catch((error) => {
+          console.error("Error during login:", error);
+          alert("Login failed. Please check your email and password.");
+        });
+    };
+
+    return (
+        <div>
+          <h2>Login</h2>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label>Email:</label>
+              <input
+                type="email"
+                name="email"
+                value={loginData.email}
+                onChange={handleInputChange}
+                required
+              />
+            </div>
+            <div>
+              <label>Password:</label>
+              <input
+                type="password"
+                name="password"
+                value={loginData.password}
+                onChange={handleInputChange}
+                required
+              />
+            </div>
+            <button type="submit">Login</button>
+          </form>
+        </div>
+      );
+    }
+    
+    export default Login;
