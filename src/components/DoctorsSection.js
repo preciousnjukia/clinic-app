d58d506e84b475da1d658464c8db83ba13116755
munene-guantai <136487831+munene-guantai@users.noreply.github.com>
2023-09-14 19:58:42 +0300

    Update DoctorsSection.js

--- src/components/DoctorsSection.js
+++ src/components/DoctorsSection.js
@@ -1,7 +1,22 @@
 import React from "react";
+import DoctorProfile from './DoctorProfile';
 
-function DoctorsSection(){
 
+function DoctorsSection({ doctors }){
+    return (
+        <div className="top-rated-doctors">
+            <h2>Top Rated Doctors Near You</h2>
+            <div className="doctor-list">
+                {doctors.map((doctor, index) => (
+                    <DoctorProfile key={index} doctor={doctor} />
+                ))}
+            </div>
+            <div className="pagination">
+                <button>&lt; Next Page</button>
+                <button>View All</button>
+            </div>
+        </div>
+    );
 }
 
 export default DoctorsSection;
