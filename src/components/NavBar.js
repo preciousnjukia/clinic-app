d53bf5adffeb481954e12b976c02503acc87e8b1
preciousnjukia <preciousnjukia@gmail.com>
2023-09-14 19:49:27 +0300

    More changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,9 +6,6 @@ function NavBar(){
     return (
     <nav className="navbar">
      <h1>Clinic App</h1>
-      <div className="navbar-logo">
-        <NavLink to="/">Clinic App</NavLink>
-      </div>
       <ul className="navbar-menu">
         <li>
           <NavLink to="/book-appointment">Book Appointment</NavLink>
