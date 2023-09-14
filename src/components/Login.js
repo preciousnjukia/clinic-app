910b583744459abe9707ec4220ecbfeba1360818
preciousnjukia <preciousnjukia@gmail.com>
2023-09-14 03:13:19 +0300

    More changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -24,7 +24,7 @@ function Login() {
         password,
       };
 
-      fetch("/src/user.json", {
+      fetch("/src/users.json", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
---
+++ src/components/Signup.js
@@ -0,0 +1,103 @@
+import React, { useState } from "react";
+
+function Signup () {
+  const [formData, setFormData] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const { username, email, password } = userRegistrationData;
+
+    const newUser = {
+        username,
+        email,
+        password,
+      };
+
+      fetch("/src/users.json", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newUser),
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          alert("Registration successful! You can now log in.");
+        })
+        .catch((error) => {
+          console.error("Error during registration:", error);
+          alert("Registration failed. Please try again later.");
+        }); 
+  };
+
+  return (
+    <div className="signup">
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="fullName">Full Name:</label>
+          <input
+            type="text"
+            id="fullName"
+            name="fullName"
+            value={formData.fullName}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="confirmPassword">Confirm Password:</label>
+          <input
+            type="password"
+            id="confirmPassword"
+            name="confirmPassword"
+            value={formData.confirmPassword}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
